7d1752b464a0c7e2a536ccd621dea3f54fb3518d
mood <chamesh@naver.com>
2022-12-07 20:00:34 +0900

    server를 listen상태, 즉 대기상태로 만들어놓음? port값과 host값을 넣어주었고, 콜백함수가 실행되도록 만들어줌. 여기서는 console.log(hello)를 찍어서 눈에 보이도록 만들어주었음

--- server.js
+++ server.js
@@ -2,4 +2,9 @@ const net = require('net')
 const PORT = process.env.SERVER_PORT || 3000
 const HOST = process.env.SERVER_HOST || "127.0.0.1"
 
-const server = net.createServer()
+const server = net.createServer(()=>{
+})
+
+server.listen(PORT, HOST, ()=>{
+    console.log("hello")
+})
