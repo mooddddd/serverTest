ff99e3c8baa793f35bc55abddcf2b0b43db8d7f1
mood <chamesh@naver.com>
2022-12-07 21:21:01 +0900

    server.js와 client.js 연결시켜줬음

--- client.js
+++ client.js
@@ -1,4 +1,16 @@
 const net = require('net')
 
 const config = {port:3000, host:'127.0.0.1'}
-const soket = net.connect(config)
+const socket = net.connect(config)
+
+socket.on('connect', ()=>{
+    console.log("connected to server!")
+
+    socket.write("hello!")
+})
+
+socket.on("data",(message)=>{
+    console.log(`Received : ${message}`)
+
+    socket.end()
+})
--- server.js
+++ server.js
@@ -2,10 +2,22 @@ const net = require('net')
 const PORT = process.env.SERVER_PORT || 3000
 const HOST = process.env.SERVER_HOST || "127.0.0.1"
 
-const server = net.createServer(()=>{
-    여기 안에 들어가는 애는, 클라이언트가 뭔가 실행됐을 때 발동됨!!
+const server = net.createServer((client)=>{
+    client.on("data", (data)=>{
+        console.log(data)
+    })
+
+    client.write("나 데이터 받았어!")
+
+    client.on("close", ()=>{
+        console.log("잘가!")
+    })
 })
 
 server.listen(PORT, HOST, ()=>{
-    console.log("대기모드")
+    console.log(`Server Listening Port : ${PORT}`)
+
+    server.on("connection", ()=>{
+        console.log("client 접속 완료")
+    })
 })
