c7d433b0b87e53b8be92f977637ccde1c9ac60fa
mood <chamesh@naver.com>
2022-12-08 10:09:55 +0900

    2교시 시작(코드 정리)

--- server.js
+++ server.js
@@ -2,33 +2,16 @@ const net = require('net')
 const PORT = process.env.SERVER_PORT || 3000
 const HOST = process.env.SERVER_HOST || "127.0.0.1"
 
-const body = Buffer.from(`<h1>hello world</h1>`)
-const res = `HTTP/1.1 200 ok
-Connection: keep-alive
-Keep-Alive: timeout=5
-Content-type: text/html
-Content-length: ${body.length}
-
-${body.toString()}
-`
-
 const server = net.createServer((client)=>{
-    client.setEncoding("utf-8")
-    client.on("data", (data)=>{
-        console.log(data)
+    client.on("data", (data)=>{ // client한테 데이터가 올 때 뒤에 있는 함수를 실행시킨다!
+        console.log(data) // client로부터 온 데이터가 찍히도록 콘솔로그 이용함
     })
+})
 
-    client.write(res)
-
-    client.on("close", ()=>{
-        console.log("잘가!")
-    })
+server.on("connection", ()=>{
+    console.log("client 접속 완료")
 })
 
 server.listen(PORT, HOST, ()=>{
     console.log(`Server Listening Port : ${PORT}`)
-
-    server.on("connection", ()=>{
-        console.log("client 접속 완료")
-    })
 })
